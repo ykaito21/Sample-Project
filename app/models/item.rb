44bf97c54823826ffd8820c98141db900205d80e
Yuki Kaito <ykaito21@gmail.com>
2018-03-03 21:13:47 +0900

    Add Items and validation to Tweets

---
+++ app/models/item.rb
@@ -0,0 +1,9 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :user, presence: true
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :text, presence: true
+
+end
